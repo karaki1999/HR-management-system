21bfca63fe3c298b211c09e73de4d08579a026c6
AL-KaraKi Omar <98954835+karaki1999@users.noreply.github.com>
2022-02-18 01:25:42 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,57 +1,83 @@
-var emp = [ ] ;
-function Employees(Employee_ID , Full_Name , Department , Level, Image ,Salary )
-{
-    this.Employee_ID= Employee_ID;
-    this.Full_Name=Full_Name;
-    this.Department= Department;
-    this.Level= Level;
-    this.Image=Image;
-    this.Salary=0;
-    emp.push(this);
+var id = 1001;
+var Obj ={
+    Name:'',
+    Dept:'',
+    Level:'',
+    Image:''
 }
+function Finish ()
+{
+    fullName();
+    employeeId();
+    department();
+    level();
+    image();
+   var confrem= valdaiton();
+   if(confrem==='ok')
+   render();
+    console.log(Obj);
+}
+function render(){
+
+
+  var mainDiv  =document.getElementById('Card');
+  var Image = document.createElement('img');
+  var Name = document.createElement('h3');
+  var department=document.createElement('h3');
+  var level = document.createElement('h3');
+  var Id =document.createElement('h3');
+  Image.classList='imageCard'
+  Image.src=Obj.Image;
+  Name.innerText=Obj.Name
+  department.innerText=Obj.Dept
+  level.innerText=Obj.Level
+  Id.innerText=id
+  mainDiv.appendChild(Image); 
+ var newDiv= document.createElement('div');
+ newDiv.append(Id);
+ newDiv.append(Name);
+ newDiv.append(department);
+ newDiv.append(level);
+ mainDiv.append(newDiv);
+
+}
+function valdaiton()
+{ 
+    var err= false;
+    var Error=[];
+  Object.keys(Obj).forEach(key=>{
+     if(Obj[key]===""){
+        Error.push(key);
+        err=true; 
+     }
+  });
+  if(err){
+    var errMessage='';
+    Error.map((d)=>errMessage+=d+',')
+    alert(errMessage+' is required')
+    return false;  
+}
+return'ok';
+}
+function employeeId()
+{
+    return id++;
+}
+function fullName() {
+   Obj.Name = document.getElementById("name").value;
+    return Obj.Name;
     
-function myFun() {
-    let Ghazi_Samer = new Employees (1000 ,"Ghazi Samer" , "Administration ", "Senior" ,"URL");
-    let Lana_Ali = new Employees (1001 ,"Lana Ali" , "Finance ", "Senior" ,"URL");
-    let Tamara_Ayoub = new Employees (1002 ,"Tamara Ayoub" , "Marketing ", "Senior" ,"URL");
-    let Safi_Walid = new Employees (1002 ,"Safi Walid" , "Administration ", "Mid-Senior" ,"URL");
-    let Omar_Zaid = new Employees (1004 ,"Omar Zaid" , "Development ", "Senior" ,"URL");
-    let Rana_Saleh = new Employees (1005 ,"Rana Saleh" , "Development ", "Junior" ,"URL");
-    let Hadi_Ahmad = new Employees (1006 ,"Hadi Ahmad" , "Finance ", "Mid-Senior" ,"URL");
-    emp.forEach(Employees => {
-        Employees.Salary =  getSalary(Employees);
-    });
-
-    emp.forEach(Employees => {
-        document.write(`<h2 style ="margin-bottom : 0px ; color: rgb(62, 6, 114) ; "> Employee Name :${Employees.Full_Name} --- Employee salary : ${Employees.Salary}</h2>`);
-    });
 }
-function getSalary(Employees)
+function department() {
+    var select = document.getElementById('department');
+    Obj.Dept = select.options[select.selectedIndex].value;
+}
+function level()
 {
-    debugger;
-    let max, min, salary, totalSalary;
-    if (Employees.Level == "Junior")
-    {
-        max = 1000;
-        min = 500;
-        salary = Math.floor(Math.random() * (max- min +1) + min);
-        totalSalary = salary * 0.75;
-        return totalSalary;
-    }
-    else if (Employees.Level == "Mid-Senior")
-    {
-        max = 1500;
-        min = 1000;
-        salary = Math.floor(Math.random() * (max- min +1) + min);
-        totalSalary = salary * 0.75;
-        return totalSalary;
-    }
-
-    else {
-        max = 2000;
-        min = 1500;
-        salary = Math.floor(Math.random() * (max- min +1) + min);
-        totalSalary = salary * 0.75;
-        return totalSalary;
-    }
+    var select = document.getElementById('level');
+    Obj.Level = select.options[select.selectedIndex].value;
+}
+function image()
+{
+    Obj.Image = document.getElementById("image").value;
 }
